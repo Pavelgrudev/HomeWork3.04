4d426719d03b41acd3a6c777afe5a9acf249f91a
Павел <Pavel-grudev@yandex.ru>
2023-06-21 07:28:46 +0300

    Branch1

---
+++ Scripts.sql.js
@@ -0,0 +1,6 @@
+select * from students;
+select * from students where age between 20 and 24;
+select name from students;
+select * from students where name Ilike '%o%';
+select * from students where age>id;
+select * from students order by age desc;
